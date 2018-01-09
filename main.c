8e5faf670b9e7f9e2b84fc29565ac15ec5cfc0f3
slembcke <slembcke@gmail.com>
2018-01-08 23:30:09 -0600

    More fun with colors.

--- main.c
+++ main.c
@@ -27,72 +27,100 @@ u8 i, ix, iy;
 u8 spr_id;
 
 static const u8 PALETTE[] = {
-	0x21, 0x0F, 0x0F, 0x20,
-	0x00, 0x0F, 0x0F, 0x2C,
-	0x00, 0x0F, 0x0F, 0x2C,
-	0x00, 0x0F, 0x0F, 0x2C,
-	
-	0x00, 0x00, 0x00, 0x36,
-	0x00, 0x00, 0x00, 0x37,
-	0x00, 0x00, 0x00, 0x38,
-	0x00, 0x00, 0x00, 0x39,
+	0x0D, 0x0F, 0x0F, 0x30,
+	0x0D, 0x0F, 0x0F, 0x05,
+	0x0D, 0x0F, 0x0F, 0x09,
+	0x0D, 0x0F, 0x0F, 0x01,
+	0x0D, 0x00, 0x00, 0x36,
+	0x0D, 0x00, 0x00, 0x37,
+	0x0D, 0x00, 0x00, 0x38,
+	0x0D, 0x00, 0x00, 0x39,
 };
 
 static const char GREET[] = "Global Game Jam 2018!!";
 
-static const s8 SIN_TABLE[] = {
-	0, 1, 3, 4, 6, 7, 8, 10, 11, 12, 13, 14, 14, 15, 15, 15, 16, 15,
-	15, 15, 14, 14, 13, 12, 11, 10, 8, 7, 6, 4, 3, 1, 0, -1, -3, -4,
-	-6, -7, -8, -10, -11, -12, -13, -14, -14, -15, -15, -15, -16, -15,
-	-15, -15, -14, -14, -13, -12, -11, -10, -8, -7, -6, -4, -3, -1,
+static const s8 SIN_TABLE[256] = {
+	0, 0, 1, 2, 3, 3, 4, 5, 6, 7, 7, 8, 9, 10, 10, 11, 12, 12, 13, 14, 15, 15, 16, 17, 17, 18, 19, 19, 20, 20,
+	21, 22, 22, 23, 23, 24, 24, 25, 25, 26, 26, 27, 27, 27, 28, 28, 28, 29, 29, 29, 30, 30, 30, 30, 31, 31, 31,
+	31, 31, 31, 31, 31, 31, 31, 32, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 29, 29, 29, 28, 28,
+	28, 27, 27, 27, 26, 26, 25, 25, 24, 24, 23, 23, 22, 22, 21, 20, 20, 19, 19, 18, 17, 17, 16, 15, 15, 14, 13,
+	12, 12, 11, 10, 10, 9, 8, 7, 7, 6, 5, 4, 3, 3, 2, 1, 0, 0, 0, -1, -2, -3, -3, -4, -5, -6, -7, -7, -8, -9,
+	-10, -10, -11, -12, -12, -13, -14, -15, -15, -16, -17, -17, -18, -19, -19, -20, -20, -21, -22, -22, -23,
+	-23, -24, -24, -25, -25, -26, -26, -27, -27, -27, -28, -28, -28, -29, -29, -29, -30, -30, -30, -30, -31,
+	-31, -31, -31, -31, -31, -31, -31, -31, -31, -32, -31, -31, -31, -31, -31, -31, -31, -31, -31, -31, -30,
+	-30, -30, -30, -29, -29, -29, -28, -28, -28, -27, -27, -27, -26, -26, -25, -25, -24, -24, -23, -23, -22,
+	-22, -21, -20, -20, -19, -19, -18, -17, -17, -16, -15, -15, -14, -13, -12, -12, -11, -10, -10, -9, -8, -7,
+	-7, -6, -5, -4, -3, -3, -2, -1, 0,
 };
 
 void main (void) {
 	ppu_off(); {
 		pal_all(PALETTE);
 		
-		vram_adr(NTADR_A(5, 4));
-		for(i = 0; i < sizeof(GREET) - 1; ++i){
-			vram_put(GREET[i]);
-		}
+		vram_adr(NTADR_A(0, 0));
+		vram_fill(128, 32*30);
 		
-		for(iy = 0; iy < 16; ++iy){
-			vram_adr(NTADR_A(8, iy + 8));
-			for(ix = 0; ix < 16; ++ix){
-				vram_put((iy << 4) | ix);
-			}
-		}
+		vram_adr(NTADR_A(4, 3));
+		vram_fill(0, sizeof(GREET) + 1);
+		vram_adr(NTADR_A(4, 4));
+		vram_fill(0, sizeof(GREET) + 1);
+		vram_adr(NTADR_A(4, 5));
+		vram_fill(0, sizeof(GREET) + 1);
+		vram_adr(NTADR_A(5, 4));
+		vram_write(GREET, sizeof(GREET) - 1);
+		
 		
 		vram_adr(NTADR_A(0, 30));
-		for(i = 0; i < 16; ++i){
-			vram_put(0xFF);
-		}
+		for(i = 0; i < 8; ++i) vram_put(0x50);
+		for(i = 0; i < 8; ++i) vram_put(0xFA);
+		for(i = 0; i < 8; ++i) vram_put(0x50);
+		for(i = 0; i < 8; ++i) vram_put(0xFA);
+		for(i = 0; i < 8; ++i) vram_put(0x50);
+		for(i = 0; i < 8; ++i) vram_put(0xFA);
+		for(i = 0; i < 8; ++i) vram_put(0x50);
+		for(i = 0; i < 8; ++i) vram_put(0xFA);
+		
+		// for(iy = 0; iy < 16; ++iy){
+		// 	vram_adr(NTADR_A(12, iy + 8));
+		// 	for(ix = 0; ix < 16; ++ix){
+		// 		vram_put((iy << 4) | ix);
+		// 	}
+		// }
 	} ppu_on_all();
 	
 	music_play(0);
 	
 	while(true){
-		static u8 t0, t1, t2, t3;
+		static u8 t0, t2, t3, t_4, t_8;
+		static u8 c0 = 0, c1 = 3, c2 = 6, c3 = 9;
 		
 		t0 = nesclock();
-		t1 = t0 >> 1;
-		t2 = t1 >> 1;
-		t3 = t2 >> 1;
+		t2 = 2*t0;
+		t3 = 3*t0;
 		
 		spr_id = 0;
-		spr_id = oam_spr(32 + SIN_TABLE[(t0 +  0) & 0x3F], 120 + SIN_TABLE[(t0 + 16) & 0x3F], '!', (t2 + 0) & 0x03, spr_id);
-		spr_id = oam_spr(32 + SIN_TABLE[(t0 +  8) & 0x3F], 120 + SIN_TABLE[(t0 + 19) & 0x3F], '8', (t2 + 1) & 0x03, spr_id);
-		spr_id = oam_spr(32 + SIN_TABLE[(t0 + 16) & 0x3F], 120 + SIN_TABLE[(t0 + 22) & 0x3F], '1', (t2 + 2) & 0x03, spr_id);
-		spr_id = oam_spr(32 + SIN_TABLE[(t0 + 24) & 0x3F], 120 + SIN_TABLE[(t0 + 25) & 0x3F], '0', (t2 + 3) & 0x03, spr_id);
-		spr_id = oam_spr(32 + SIN_TABLE[(t0 + 32) & 0x3F], 120 + SIN_TABLE[(t0 + 28) & 0x3F], '2', (t2 + 0) & 0x03, spr_id);
-		spr_id = oam_spr(32 + SIN_TABLE[(t0 + 40) & 0x3F], 120 + SIN_TABLE[(t0 + 31) & 0x3F], 'J', (t2 + 1) & 0x03, spr_id);
-		spr_id = oam_spr(32 + SIN_TABLE[(t0 + 48) & 0x3F], 120 + SIN_TABLE[(t0 + 34) & 0x3F], 'G', (t2 + 2) & 0x03, spr_id);
-		spr_id = oam_spr(32 + SIN_TABLE[(t0 + 56) & 0x3F], 120 + SIN_TABLE[(t0 + 37) & 0x3F], 'G', (t2 + 3) & 0x03, spr_id);
+		spr_id = oam_spr(128 + SIN_TABLE[(t3 + 0x00) & 0xFF], 120 + SIN_TABLE[(t2 + 0x40) & 0xFF], '!', 0, spr_id);
+		spr_id = oam_spr(128 + SIN_TABLE[(t3 + 0x10) & 0xFF], 120 + SIN_TABLE[(t2 + 0x50) & 0xFF], '8', 1, spr_id);
+		spr_id = oam_spr(128 + SIN_TABLE[(t3 + 0x20) & 0xFF], 120 + SIN_TABLE[(t2 + 0x60) & 0xFF], '1', 2, spr_id);
+		spr_id = oam_spr(128 + SIN_TABLE[(t3 + 0x30) & 0xFF], 120 + SIN_TABLE[(t2 + 0x70) & 0xFF], '0', 3, spr_id);
+		spr_id = oam_spr(128 + SIN_TABLE[(t3 + 0x40) & 0xFF], 120 + SIN_TABLE[(t2 + 0x80) & 0xFF], '2', 0, spr_id);
+		spr_id = oam_spr(128 + SIN_TABLE[(t3 + 0x60) & 0xFF], 120 + SIN_TABLE[(t2 + 0xA0) & 0xFF], 'J', 1, spr_id);
+		spr_id = oam_spr(128 + SIN_TABLE[(t3 + 0x70) & 0xFF], 120 + SIN_TABLE[(t2 + 0xB0) & 0xFF], 'G', 2, spr_id);
+		spr_id = oam_spr(128 + SIN_TABLE[(t3 + 0x80) & 0xFF], 120 + SIN_TABLE[(t2 + 0xC0) & 0xFF], 'G', 3, spr_id);
 		oam_hide_rest(spr_id);
 		
-		// sfx_play(0, 0);
+		if((t0 & 0x3) == 0){
+			++c0; if(c0 >= 12) c0 = 0;
+			++c1; if(c1 >= 12) c1 = 0;
+			++c2; if(c2 >= 12) c2 = 0;
+			++c3; if(c3 >= 12) c3 = 0;
+			pal_col( 3, 0x11 + c0);
+			pal_col( 7, 0x11 + c1);
+			pal_col(11, 0x11 + c2);
+			pal_col(15, 0x11 + c3);
+		}
 		
-		pal_col(0, 0x01 + (t3 & 0x07));
+		scroll(0, 240 + (SIN_TABLE[t2] >> 2));
 		
 		ppu_wait_nmi();
 	}
