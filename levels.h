b5d6e56a7e5e88a64f3058b718fe172d8f18a23d
Ben Gartner <ben.gartner@milestone.com>
2018-01-27 17:48:39 -0600

    levels.h

---
+++ levels.h
@@ -0,0 +1,17 @@
+#ifndef LEVELS_H__
+#define LEVELS_H__
+
+const u16 level_0[8] = {
+    0xF00F,
+    0xF10F,
+    0xF04F,
+    0xF23F,
+    0xF2CF,
+    0xF0CF,
+    0xF00F,
+    0xF00F
+};
+const u8 level_0_x = 6;
+const u8 level_0_y = 6;
+
+#endif //#ifndef LEVELS_H__
