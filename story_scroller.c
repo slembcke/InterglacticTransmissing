8dcf44396e8c77d36920623d1ab77ee2a29be364
Ben Gartner <ben.gartner@milestone.com>
2018-01-28 14:58:26 -0600

    Joke story and fading in

--- story_scroller.c
+++ story_scroller.c
@@ -1,6 +1,7 @@
 #include <joystick.h>
 #include <nes.h>
 #include "neslib/neslib.h"
+#include <string.h>
 
 #include "main.h"
 
@@ -8,29 +9,51 @@ static const char *TEXT[] = {
 	"In amother galaxy. A transm-",
 	"issing was sending...",
 	"",
-	"In case we don't have time",
-	"to add more story, use your",
-	"imagination please.",
+	"Critical data is stranded.",
+	"Your mission is to keep the",
+	"signal alive.",
+	"",
+	"Asteriods and Garfon IX",
+	"voidcrafts block progress",
+	"",
+	"No one will",
+	"hold failure against you.",
+	"",
+	"That's just not the culture",
+	"here.",
+	"",
+	"But still.",
 	NULL,
 };
 
 TAIL_CALL story_scroller_start(){
 	fade_out();
 	ppu_off(); {
-		oam_clear();
-		
+	 	oam_clear();
 		vram_inc(0);
 		vram_adr(NAMETABLE_A);
 		vram_fill(0x00, 32*32);
-		
-		for(iy = 0; TEXT[iy]; ++iy){
-			const char *str = TEXT[iy];
+		fade_in();
+
+	} ppu_on_all();
+
+	for(iy = 0; TEXT[iy]; ++iy){
+		const char *str = TEXT[iy];
+		ppu_off(); {
+			
 			vram_adr(NTADR_A(2, iy + 2));
 			for(ix = 0; str[ix]; ++ix) vram_put(str[ix]);
+			
+		} ppu_on_all();
+		if(0==strlen(TEXT[iy])) {
+			// Wait for start clear.
+			while(JOY_START(joy_read(0))){}
+			
+			// Wait for start pressed.
+			while(!JOY_START(joy_read(0))){}
 		}
-		
-	} ppu_on_all();
-	fade_in();
+
+	}
 	
 	// Wait for start clear.
 	while(JOY_START(joy_read(0))){}
